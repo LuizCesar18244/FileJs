d3ad484041679f0f8cbee5fd518f04fed5575902
Unknown <luizcesarmais44@hotmail.com>
2019-02-24 17:56:24 -0300

    Estilizando input e validando o Button

--- JavaScript/send.js
+++ JavaScript/send.js
@@ -1,5 +1,22 @@
-let input = document.querySelector('.inputfile');
 
-input.addEventListener('change', function(e) {
-	console.log('Mudou');
+let button = document.querySelector( '.button' );
+button.disabled = true;
+
+document.querySelector( '#file' ).addEventListener( 'change', function( e ) {
+	
+	let file = this.files[0];
+	
+	document.querySelector( '#label-file' ).textContent = file.name;
+
+	if( file )
+	{
+		button.classList.add( 'enable-button' );
+		button.disabled = false;
+
+		button.addEventListener( 'click', function( e ) {
+		 let xhr = new XMLHttpRequest ( );
+		console.log( xhr );
+	});
+
+	}
 });
